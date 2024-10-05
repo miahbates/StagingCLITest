474083f583e44aa38f5c31dd4132e0c2af94c400
miahbates <miahbates@icloud.com>
2024-10-05 11:14:27 +0100

    staging environment three

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ export default function Home() {
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
         <ul className="list-inside text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">Test staging environment 1 💃</li>
+          <li className="mb-2">Test staging environment 3 💅</li>
         </ul>
       </main>
     </div>
