8b14c9dd4bc2973fc9acc4ac9503f4bfb0b32460
miahbates <miahbates@icloud.com>
2024-10-05 11:52:43 +0100

    env

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,8 @@ export default function Home() {
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
         <ul className="list-inside text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">Test staging environment 4 🪭</li>
+          <li className="mb-2">Staging Four</li>
+          <li className="mb-2">Foo: {process.env.FOO}</li>
         </ul>
       </main>
     </div>
